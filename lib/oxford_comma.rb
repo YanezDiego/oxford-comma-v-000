cd66a9e347b0466536f18cdc7002449933fdf460
Diego Yanez <diegosyanez92@gmail.com>
2018-03-28 15:27:01 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length < 2
     return "#{array[1]} and #{array[2]}"
   else array.length > 2
-    return "#{array[0]}"
+    #return "#{array[0]}"
 
 
     #return= "#{array[0, array.length].join(', ')} and #{array.last}"
