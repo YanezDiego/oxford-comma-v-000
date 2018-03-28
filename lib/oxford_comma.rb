c0ec4b365a508588208d2c96a0f9cad95457efcf
Diego Yanez <diegosyanez92@gmail.com>
2018-03-28 15:04:54 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,9 @@
 def oxford_comma(array)
   if array.length == 1
     result = array[0]
-  else
+  elsif array == 2
+    array.join (" and ")
+
     result = "#{array[0, array.length].join(', ')} and #{array.last}"
   end
 end
